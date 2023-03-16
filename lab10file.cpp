514ed37ad083d8edb6544d61e460c4d763261ee5
Zackary9785 <roodlesemail@gmail.com>
2023-03-16 10:22:22 -0700

    added lab10file.cpp

---
+++ lab10file.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void name();
+
+int main()
+{
+    name();
+    return 0;
+}
+
+void name()
+{
+    cout<<"Hello, my name is Zackary"<<endl;
+}
